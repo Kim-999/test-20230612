e7f1f1b1baf77c92c1c35d1d753ac712c8837a9b
JBEDU29 <JBEDU29@DESKTOP-HMF1SKG>
2023-06-12 12:41:31 +0900

    수정 사항

--- src/main/java/com/company/exer/Testcontroller.java
+++ src/main/java/com/company/exer/Testcontroller.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.company.exer.dto.MemberDto;
+
 @Controller
 public class Testcontroller {
 	
